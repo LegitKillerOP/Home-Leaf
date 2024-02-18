4616d428a3da8edb7797b4035890bb0f180982e2
Legit Killer <91422151+LegitKillerOP@users.noreply.github.com>
2024-02-18 18:25:56 +0530

    Update homepage CSS and JS, add scroll-up button

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -17,3 +17,14 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 4000);
 }
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1000) {
+        $('.scroll-up-btn').addClass(" show");
+        console.log('test1');
+    }
+});
+$('.scroll-up-btn').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+});
